facd4a262666a3f30243b4784ef3e2c269152774
Pedro Miguel <pedromrap@gmail.com>
2022-07-20 21:50:51 -0300

    feat: unifying icons components exports

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,17 @@
+import { CompanyIcon } from './Company';
+import { LinkIcon } from './Link';
+import { LocationIcon } from './Location';
+import { TwitterIcon } from './Twitter';
+import { MoonIcon } from './Moon';
+import { SunIcon } from './Sun';
+import { SearchIcon } from './Search';
+
+export {
+  CompanyIcon,
+  LinkIcon,
+  LocationIcon,
+  TwitterIcon,
+  MoonIcon,
+  SunIcon,
+  SearchIcon,
+};
